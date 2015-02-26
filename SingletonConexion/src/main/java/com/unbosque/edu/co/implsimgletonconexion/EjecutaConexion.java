604c17ff4a82788a9d9e054834aedb877a665ac8
Ana Maria <>
2015-02-26 00:31:08 -0500

    Se ejecuta query  para comprobar conexion en Bd remota implementando singleton

--- SingletonConexion/src/main/java/com/unbosque/edu/co/implsimgletonconexion/EjecutaConexion.java
+++ SingletonConexion/src/main/java/com/unbosque/edu/co/implsimgletonconexion/EjecutaConexion.java
@@ -1,37 +1,62 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package com.unbosque.edu.co.implsimgletonconexion;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Grupo 1
  */
 public class EjecutaConexion {
-
+    
     public static void main(String[] args) {
-
+        
+        
         SingletonConexion conexion = SingletonConexion.getInstance("jdbc:postgresql://nuevo.aretico.com:5432/software_2", "grupo1", "jDsX9eE08DSAOWehhCnD");
-
+        
         System.out.println(conexion.getUrl());
-
+        
         System.out.println(conexion.getUser());
         
         System.out.println(conexion.getPass());
         
+        Connection connection = null;
         try {
-            Connection con = conexion.getConnection();
-            if (con != null) {
+            connection = conexion.getConnection();
+            if (connection != null) {
                 System.out.println("Conexion Exitosa");
             } else {
                 System.out.println("Conexion Erronea");
-            }    
+            }
         } catch (Exception e) {
             System.out.println("Error de conexion: " + e.getMessage());
+        }finally{
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SingletonConexion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        try {
+            ResultSet resultSet = conexion.executeQuery("select now()");
+            if (resultSet.next()) {
+                Date fechaActual = (Date) resultSet.getDate(1);
+                System.out.println("La ejecucion del query resulta --> " + fechaActual);
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(EjecutaConexion.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
--- SingletonConexion/src/main/java/com/unbosque/edu/co/implsimgletonconexion/SingletonConexion.java
+++ SingletonConexion/src/main/java/com/unbosque/edu/co/implsimgletonconexion/SingletonConexion.java
@@ -1,13 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package com.unbosque.edu.co.implsimgletonconexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,42 +18,42 @@ import java.util.logging.Logger;
  * @author Grupo 1
  */
 public class SingletonConexion {
-
+    
     private final String url;
     private final String user;
     private final String pass;
     private static SingletonConexion miconfigurador;
-
+    
     private SingletonConexion(String url, String user, String pass) {
-
+        
         this.url = url;
         this.user = user;
         this.pass = pass;
-
+        
     }
-
+    
     public static SingletonConexion getInstance(String url, String user, String pass) {
-
+        
         if (miconfigurador == null) {
-
+            
             miconfigurador = new SingletonConexion(url, user, pass);
         }
         return miconfigurador;
     }
-
+    
     public String getUrl() {
         return url;
     }
-
+    
     public String getUser() {
         return user;
     }
-
-
+    
+    
     public String getPass() {
         return pass;
     }
-
+    
     
     public Connection getConnection() throws Exception {
         
@@ -69,5 +71,28 @@ public class SingletonConexion {
         
         return connection;
     }
-
+    
+    public ResultSet executeQuery(String query) {
+        ResultSet rest = null;
+        Connection connection = null;
+        Statement stm = null;
+        try {
+            connection = getConnection();
+            stm = connection.createStatement();
+            rest = stm.executeQuery(query);           
+            
+        } catch (Exception ex) {
+            Logger.getLogger(SingletonConexion.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(SingletonConexion.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return rest;
+    }
+    
 }
